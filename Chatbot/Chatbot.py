d07eaf60f469a48f7ea5749aedfde2e874ea6dd0
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-28 12:46:31 +0000

    Chatbot save 3
    Third save of Chatbot

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -16,8 +16,14 @@ print(random_greeting + " " + response)
 
 response = raw_input(random_question)
 
-while True:
-    userInput = raw_input()
-    if userInput in sad:
-        print("Sorry to hear that")
+userInput = response
+
+
+if userInput in sad:
+        print("Sorry to hear that.")
+if userInput in mixed:
+        print("Good, at least it wasn't a bad day.")
+if userInput in happy:
+        print("Brilliant, thats nice to hear.")
+        
     
