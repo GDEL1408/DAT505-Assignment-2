8bf91c3fbe2197b1da169084b086066e9828df44
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-29 13:10:44 +0000

    Webservice save 3
    Third save of Webservice

--- Webservice/webService.py
+++ Webservice/webService.py
@@ -1,12 +1,14 @@
 import twitter, datetime
 
-file = open("C:\Users\George\Documents\Uni Work\Year 2\DAT505\Assignment 2\Webservice\TwitterCredentials.txt")
+file = open("C:\Users\George\Documents\Uni Work\Year 2\DAT505\Assignment 2\Webservice\TwitterCredentials.txt", 'r')
 creds = file.readline().strip().split(',')
 
 api = twitter.Api(creds[0],creds[1],creds[2],creds[3])
 
 timestamp = datetime.datetime.utcnow()
 
-response = api.postupdate("Tweeted at " + str(timestamp))
+response = api.PostUpdate("Tweeted at " + str(timestamp))
+
+file = open("C:\Users\George\Library\")
 
 print("Status updated to: " + response.text)
