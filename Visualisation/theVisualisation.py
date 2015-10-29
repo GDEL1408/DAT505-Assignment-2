570b6d3ca29fa314e80483f758b66aff77ff9e9e
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-29 13:10:06 +0000

    visualisation save 2
    Second save of visualisation

--- Visualisation/theVisualisation.py
+++ Visualisation/theVisualisation.py
@@ -2,17 +2,24 @@ from graphics import *
 
 window = GraphWin("Visualisation", 750, 750)
 
+file = open("C:\Users\George\Documents\GitHub\DAT505-Assignment-2\Visualisation\data.txt")
+scores = file.readline().strip().split('.')
+
 #setBackground(color_rgb(255, 255, 255))
-line = line(point(200, 200), point(200,550))
-line.setfill(color_rgb(255, 0, 0))
-line.draw(window)
+ball = circle(point(200, 200), point(200,550))
+ball.setfill(color_rgb(255, 0, 0))
+ball.draw(window)
 xspeed = 2
 
 while True:
-    currentPos = line.getP2()
+    currentPos = line.getcenter()
     if(currentPos.getX() >= 700):
-        yspeed = -yspeed
-    if(currentPos.getP1() >= 0):
         xspeed = -xspeed
+    if(currentPos.getX() >= 0):
+        xspeed = -xspeed
+    if(currentPos.getY() >= 700):
+        Yspeed = -Yspeed
+    if(currentPos.getY() >= 0):
+        Yspeed = -Yspeed
         
 window.getMouse()
