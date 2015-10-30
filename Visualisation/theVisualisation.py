02fe30d8c7fa69a23e5fe54a14cef0de382785a9
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-30 00:19:51 +0000

    Visualisation save 3
    Third save of Visualisation

--- Visualisation/theVisualisation.py
+++ Visualisation/theVisualisation.py
@@ -1,25 +1,49 @@
 from graphics import *
+import random, time
 
-window = GraphWin("Visualisation", 750, 750)
+file = open("data.txt",'r')
+size = file.read().splitlines()
+n = len(size)
+i = 0
+#print(n)
 
-file = open("C:\Users\George\Documents\GitHub\DAT505-Assignment-2\Visualisation\data.txt")
-scores = file.readline().strip().split('.')
+window = GraphWin("Visualisation", 700, 700)
 
-#setBackground(color_rgb(255, 255, 255))
-ball = circle(point(200, 200), point(200,550))
-ball.setfill(color_rgb(255, 0, 0))
-ball.draw(window)
-xspeed = 2
 
-while True:
-    currentPos = line.getcenter()
-    if(currentPos.getX() >= 700):
-        xspeed = -xspeed
-    if(currentPos.getX() >= 0):
-        xspeed = -xspeed
-    if(currentPos.getY() >= 700):
-        Yspeed = -Yspeed
-    if(currentPos.getY() >= 0):
-        Yspeed = -Yspeed
+for i in range(0, n):
+    #print(i)
+    r = float(size[i])
+    colormark = None
+    
+    X = random.randrange(200, 500)
+    Y = random.randrange(200, 500)
+    
+    #print("50",size[i])
+    
+    if r < 40:
+        colormark = color_rgb(255,0,0)
+        #print('low')
+    
+    elif r > 40 and r < 70:
+        colormark = color_rgb(255,255,0)
+        #print('mid')
+    
+    else: 
+        colormark = color_rgb(0,255,0)
+        #print('high')
+    
+    ball = Circle(Point(X, Y), r)
+    ball.setFill(colormark)
+    ball.draw(window)
         
-window.getMouse()
+    X = None
+    Y = None
+        
+    i = i + 1
+    
+    time.sleep(0.5)
+    
+    ball.undraw()
+
+#window.getMouse()
+window.close()
