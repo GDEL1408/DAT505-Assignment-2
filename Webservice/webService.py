896eab78c8d77832b23be618eb083bab098a6ddd
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-30 10:16:31 +0000

    Webservice Final save
    The final save of Webservices code

--- Webservice/webService.py
+++ Webservice/webService.py
@@ -12,5 +12,4 @@ response = api.PostUpdate("Tweeted at " + str(timestamp))
 file = open("C:\Users\George\AppData\Local\Google\Chrome\User Data\Default\Current Session", 'r')
 currentSession = file.readline().strip()
 
-
 print("Status updated to: " + response.text)
