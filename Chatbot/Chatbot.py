5dfbe623fd616ca49103ad92948bf2cf60971916
GDEL1408 <george.delaney@students.plymouth.ac.uk>
2015-10-30 00:22:01 +0000

    Chatbot save 6
    Sixth save of Chatbot

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,18 +1,28 @@
-import random, urllib
+import random, urllib2
+from nltk.corpus import stopwords
 
 url = "http://open.live.bbc.co.uk/weather/feeds/en/2640194/3dayforecast.rss"
+f = urllib2.urlopen(url).read()
 
+data = f.split('<title>')
+data = data[4]
+data = data.split('</title>')
+
+cachedStopWords = stopwords.words("english")
+weather = data[0]
 greetings = ['Hey', 'Hello', 'Hi']
 random_greeting = random.choice(greetings)
 questions = ['How has your day been so far?','Whats your day been like?']
 random_question = random.choice(questions)
-sad = ['bad', 'crap', 'shitty', 'boring', 'poor']
+sad = ['bad', 'crap', 'shitty', 'boring', 'poor', 'shit']
 mixed = ['okay', 'alright', 'so so', 'not bad']
 happy = ['brilliant', 'good', 'exciting', 'excellent']
 yes = ['yes', 'yeah', 'why not', 'y', 'sure', 'alright']
 no = ['no', 'nah', 'im good', 'your alright', 'nope', 'rather not']
 good = ['sun', 'sunny', 'clear', 'dry']
 bad = ['rain', 'bad', 'cloudy', 'overcast', 'fog']
+
+filtered_words = [word for word in word_list if word not in stopwords.words('english')]
 
 response_name = raw_input(random_greeting + " I'm Chatbot, " + "What is your name?")
 print(random_greeting + " " + response_name)
@@ -25,14 +35,16 @@ if userInput_question in mixed:
         print("Good, at least it wasn't a bad day.")
 if userInput_question in happy:
         print("Brilliant, thats nice to hear.")
-else print("Im not sure what you mean, sorry.")
 
 response_weather = raw_input("How is the weather today?")
 
 response_weather = raw_input("Can I tell you about the weather near you tomorrow?")
 userInput_weather = response_weather
 if userInput_weather in yes:
-        print("Okay, the weather in Plymouth tomorrow is" + put weather here + " and the Tempurature will be a high of " + maxtemp + ", low of " + mintemp)
+    print("Okay, the weather in Plymouth on " + weather)
         #put in line of text to say if they should enjoy the day whilst it lasts or tell them it will get better
-if userInput_weather in no:
+    if userInput_weather in no:
         print("Oh, okay then.")
+        
+
+
